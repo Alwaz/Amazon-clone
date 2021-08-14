87d7a9b46f4d5df114ed00611581bb02c65280cf
Alwaz <alwazkazi3@gmail.com>
2021-08-14 15:49:19 -0700

    Add to cart button added

---
+++ firebase.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyBjL753pmXUc42mM8BXrVZFg_2TNpM9w38",
+  authDomain: "clone-fdb86.firebaseapp.com",
+  projectId: "clone-fdb86",
+  storageBucket: "clone-fdb86.appspot.com",
+  messagingSenderId: "125747355386",
+  appId: "1:125747355386:web:f05ea0ac2565c06dda7343",
+  measurementId: "G-7Y2GL03NBC",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
